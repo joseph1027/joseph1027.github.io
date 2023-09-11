a44e3a07541b287b074e1250184bcb9f80866422
joseph1027 <joseph102712080104@gmail.com>
2023-09-10 18:09:28 -0700

    React init commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ import ProfileImg from './img/profile_img_1024x1024.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ProfileImg} className="ProfileImg" alt="image" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+      <header>
+      
       </header>
+
+      <body>
+        <div className='Container'>
+          <img src={ProfileImg} className="ProfileImg" alt="image" />
+        </div>
+      </body>
     </div>
   );
 }
